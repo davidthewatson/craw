a1beece7c3f3824c0d1ed2e83c68c9cd9bfaaffd
david watson <davidthewatson@gmail.com>
2017-01-09 21:47:02 -0500

    Add material admin

--- fish/pages/admin.py
+++ fish/pages/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from pages.models import WebPage
+
+
+class WebPageAdmin(admin.ModelAdmin):
+    list_display = ('url', 'links')
+
+
+admin.site.register(WebPage, WebPageAdmin)
